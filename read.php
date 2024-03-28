d6d9a5fdde97b0d3609c7d6edf7e5ca6972eb685
calthydesign <calthy.design@gmail.com>
2024-03-28 22:24:04 +0900

    修正

--- read.php
+++ read.php
@@ -13,8 +13,21 @@ $filename = 'data.csv';
 $lines = file($filename);
 // ラベル配列
 $labels = array("日付", "体調不良の有無", "症状", "メモ");
- 
+
+//配列に変換し、JSON形式にエンコード,フォームデータが存在する場合のみ値を代入
+// $unwell = isset($_POST['unwell']) ? $_POST['unwell'] : '';
+// $symptoms = isset($_POST['symptoms']) ? $_POST['symptoms'] : '';
+// $memo = isset($_POST['memo']) ? $_POST['memo'] : '';
+
+// $result = array($unwell, $symptoms, $memo);
+// $json = json_encode($result);
 ?>
+
+<!-- <script>
+    //js処理
+    const data = JSON.parse('<?=$json?>');
+    console.log(data);
+</script> -->
 
 <!-- 表形式で表示 -->
 <table>
